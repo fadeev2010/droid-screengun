e3bcd1cab9486fb522764514931e16ab6b3ecfbf
fadeev2010 <fadeev2010@gmail.com>
2014-12-23 23:01:09 +0300

    beta2

--- template/default/screengun.sh
+++ template/default/screengun.sh
@@ -1,23 +1,16 @@
 #!/bin/bash
 
 #server with a specific DISPLAY
-vncserver :11 -geometry 1024x2556
+vncserver :11 -geometry 1280x2556
 
-# export DISPLAY=:11
-# google-chrome --user-data-dir=@DISPLAY http://internet.ya.ru > /dev/null 2> /dev/null &
-# sleep 2
-# xdotool key F11
-# sleep 1
-# xdotool key F11
-sleep 3
+# sleep 3
 # killall google-chrome
 
 ## read URLs from a data file in a loop
-#count=1
 while read url name
 do
     export DISPLAY=:11
-    google-chrome --window-size=1024,2556 --user-data-dir=stage-profile --kiosk --app=http://$url > /dev/null 2> /dev/null &
+    google-chrome --window-size=1282,2056 --window-position=0,0 --user-data-dir=stage-profile --kiosk --app=http://$url > /dev/null 2> /dev/null &
 
     sleep 3
     import -display :11 -window root ../../screenshots/$name
