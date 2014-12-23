c50c55cb5dac93965199c190f40a8c4e07658385
fadeev2010 <fadeev2010@gmail.com>
2014-12-23 21:56:05 +0300

    added window size

--- template/default/screengun.sh
+++ template/default/screengun.sh
@@ -1,66 +1,34 @@
 #!/bin/bash
 
-#ENA_F11=true
-#XDO=%1
-
 #server with a specific DISPLAY
-vncserver :11 -geometry 1280x1024
+vncserver :11 -geometry 1024x2556
 
-export DISPLAY=:11
-google-chrome --tmp-profile http://internet.ya.ru > /dev/null 2> /dev/null &
-sleep 2
-xdotool key F11
-sleep 1
-xdotool key F11
-sleep 2
+# export DISPLAY=:11
+# google-chrome --user-data-dir=@DISPLAY http://internet.ya.ru > /dev/null 2> /dev/null &
+# sleep 2
+# xdotool key F11
+# sleep 1
+# xdotool key F11
+sleep 3
 # killall google-chrome
 
 ## read URLs from a data file in a loop
 #count=1
 while read url name
 do
-
-    #export DISPLAY=:11
-    #xdotool key Control+F4
-    # send URL to the firefox session
-#    firefox -private -new-tab --display :11 $url > /dev/null 2> /dev/null &
-#    chromium-brouser -new-tab --display :11 $url > /dev/null 2> /dev/null &
-   
     export DISPLAY=:11
-    google-chrome --tmp-profile --kiosk http://$url > /dev/null 2> /dev/null &
-    sleep 1
-    #export DISPLAY=:11
-    #xdotool key F11
-    #sleep 2
-    #XDO=$(xdotool getactivewindow)
-    #print "XDO=$XDO"
+    google-chrome --window-size=1024,2556 --user-data-dir=stage-profile --kiosk --app=http://$url > /dev/null 2> /dev/null &
 
-    #sleep 1
-    #if [ $ENA_F11 = "true" ]; then 
-    	#export DISPLAY=:11
-    	#wmctrl -a chrom
-    	#xdotool key F11
-    #    ENA_F11=false;
-    #fi       
-    # take a picture after waiting a bit for the load to finish
     sleep 3
     import -display :11 -window root ../../screenshots/$name
 
-    killall google-chrome
+    killall chrome
     sleep 1
-    # export DISPLAY=:11
-    #WID=`xdotool search "Mozilla Firefox" | head -1`
-    #xdotool windowactivate --sync $WID
     
-    #xdotool windowactivate $XDO
-    #wmctrl -a chrom
-    #xdotool key Control+F4 
-    #count=`expr $count + 1`
-    
-    #export DISPLAY=:11
-    #google-chrome --tmp-profile --app=http://mail.ru > /dev/null 2> /dev/null &
-
 done < url_list.txt
+
+    # export DISPLAY=:11
+    # google-chrome --user-data-dir=stage-profile internet.ya.ru > /dev/null 2> /dev/null &
 
 ## clean up when done
 vncserver -kill :11
