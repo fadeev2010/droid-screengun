0ec09518c6acc9031d9cf97fd409cb25f99cfae4
fadeev2010 <fadeev2010@gmail.com>
2014-12-24 14:09:11 +0300

    beta4 added google chrome, metadata, path, small fix

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'droid-screengun'
+maintainer 'DroidLabs LLC, Fadeev Sergey'
+source_url 'https://github.com/fadeev2010/droid-screengun.git'
+version '0.0.4'
+
+supports 'ubuntu'
+depends 'imagemagic'
--- recipes/defualt.rb
+++ recipes/defualt.rb
@@ -1,12 +1,42 @@
  
-['gnome-core', 'xfce4', 'firefox', 'vnc4server', 'xdotool', 'wmctrl'].each do |pkg|
+# add google chrome to repository
+apt_repository 'chrome' do
+  uri node['chrome']['apt_uri']
+  distribution 'stable'
+  components %w(main)
+  key node['chrome']['apt_key']
+  action :nothing
+end.run_action(:add)
+
+# Install google chrome
+package "google-chrome-#{node['chrome']['track']}" do
+  action :nothing
+end.run_action(:install)
+
+# Install component for screengun
+['gnome-core', 'xfce4', 'vnc4server'].each do |pkg|
   package pkg
 end
 
-template '/data/pa_app_user/app/shared' do
-  source 'screengun.sh'
+["#{node['screengun']['install_path']}", "#{node['screengun']['image']}"].each do |dir_create|
+  directory dir_create do
+    owner node['screengun']['user']
+    group node['screengun']['user']
+    mode '0770'
+    action :create
+  end
 end
 
-template '/data/pa_app_user/app/shared' do
-  source 'url_list.txt'
+template "#{node['screengun']['install_path']}/screengun.sh" do
+  source 'screengun.sh'
+  mode '0770'
+  variables(
+     path: node['screengun']['image_path']
+  )
+end
+
+template "#{node['screengun']['install_path']}/url_list.txt" do
+  source 'url_list.txt.erb'
+  owner node['screengun']['user']
+  group node['screengun']['user']
 end
