05ef2e0a0b48581aa1400394634bb35415384a32
fadeev2010 <fadeev2010@gmail.com>
2015-07-07 13:43:53 +0300

    fix size, readme, bump version

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
 name 'droid-screengun'
 maintainer 'DroidLabs LLC, Fadeev Sergey'
 # source_url 'https://github.com/fadeev2010/droid-screengun.git'
-version '0.0.5'
+version '0.0.6'
 
 supports 'ubuntu'
 depends 'imagemagick'
